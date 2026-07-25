3b0bf7ce001aae1975f3b0236dbf49521321d0de
gana4141 <ganeshpagar288@gmail.com>
2026-07-25 12:08:41 +0530

    Repo Updated

--- SpringRestProj07-CURDOpeartionUsingRest/src/main/java/com/nt/repo/ITouristRepo.java
+++ SpringRestProj07-CURDOpeartionUsingRest/src/main/java/com/nt/repo/ITouristRepo.java
@@ -1,14 +1,17 @@
 package com.nt.repo;
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.nt.entity.Tourist;
 
 import jakarta.transaction.Transactional;
+@Repository
 public interface ITouristRepo extends JpaRepository<Tourist, Integer>
 {
 	@Query("from Tourist where city in (:city1,:city2,:city3)order by name asc")
